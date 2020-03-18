7bb2d0dde95602d69ce30c5c283ed540fb0cecbd
Hiep <maiquanghiepdl@gmail.com>
2020-03-18 15:03:59 +0900

    update test create pod by default value

--- k8s_client.py
+++ k8s_client.py
@@ -9,13 +9,36 @@ class K8s:
         self.client = client
         self.v1Api = client.CoreV1Api()
     def create_pod(self):
-        namespace = 'viscnet'
+        # Default value. TODO: get values from request
+        namespace = 'vicsnet'
+        name = "testPythonClient"
+        image = "192.168.103.250:5000/icn-dtn-base-0.6.5:1.0"
+        commands = ["/bin/bash", "-c", "/root/start_vicsnf.sh; sleep 30d;"]
+        env = self.client.V1EnvVar("LC_ALL", "C.UTF-8")
+
+        # Create a body which stores the information of the pod to create
         body = self.client.V1Pod()
+
+        # Specify meta of a POD. TODO: specify default ip and networks in medata
+        body.metadata = client.V1ObjectMeta(namespace=namespace, name=name)
+
+
+        """
+         Specify spec including: 
+            - containers (name, image, env, command) https://github.com/kubernetes-client/python/blob/master/kubernetes/docs/V1Container.md 
+            - nodeSelector 
+        """
+        container = self.client.V1Container(command=commands, image=image, env=env)
+        node_selector = { "kubernetes.io/hostname": "vicsnet-edge1"}
+        body.spec = self.client.V1PodSpec(containers= [container], node_selector=node_selector)
+
+
         try:
             api_response = self.v1Api.create_namespaced_pod(namespace, body)
             print(api_response)
         except ApiException as e:
             print("Exception when calling CoreV1Api->create_namespaced_pod: %s\n" % e)
+
     
 test_create_pod = K8s()
 test_create_pod.create_pod()
