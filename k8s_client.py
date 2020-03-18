686677d61ec8289bdd8fd860eb48ff512d0e9375
Hiep <maiquanghiepdl@gmail.com>
2020-03-18 14:31:19 +0900

    Test create pod

--- k8s_client.py
+++ k8s_client.py
@@ -1,10 +1,21 @@
 from kubernetes import client, config
-
+from kubernetes.client.rest import ApiException
 # Configs can be set in Configuration class directly or using helper utility
-config.load_kube_config()
 
-v1 = client.CoreV1Api()
-print("Listing pods with their IPs:")
-ret = v1.list_pod_for_all_namespaces(watch=False)
-for i in ret.items:
-    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+
+class K8s:
+    def __init__(self):
+        config.load_kube_config()
+        self.client = client
+        self.v1Api = client.CoreV1Api()
+    def create_pod(self):
+        namespace = 'viscnet'
+        body = self.client.V1Pod()
+        try:
+            api_response = self.v1Api.create_namespaced_pod(namespace, body)
+            print(api_response)
+        except ApiException as e:
+            print("Exception when calling CoreV1Api->create_namespaced_pod: %s\n" % e)
+    
+test_create_pod = K8s()
+test_create_pod.create_pod()
